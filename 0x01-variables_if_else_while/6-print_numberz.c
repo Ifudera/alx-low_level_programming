41f18c3235e713c5ae10dd2cc306d35465c5feae
Ifudera <angelifunanya2001@gmail.com>
2023-07-17 16:07:39 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main -Prog Entry point
- * Return: Always 0 
+ * Return: Always 0
  */
 
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 
 	c = 0;
 	while
-		(c <10) {
+		(c < 10) {
 			putchar(c + '0');
 			c++;
 		}
