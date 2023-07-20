70bc7fdd89970a330ef297295c8f7344be3f37c4
Ifudera <angelifunanya2001@gmail.com>
2023-07-20 13:59:58 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - functs for checks for a digit (0 success).
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
