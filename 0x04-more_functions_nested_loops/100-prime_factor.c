cc4117d59426f1cf3540703d0f5ff672c4548f53
Ifudera <angelifunanya2001@gmail.com>
2023-07-21 03:18:02 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,6 +12,21 @@ int main(void)
 	long prime = 612852475143, divisor;
 
 	while (divisor < (prime / 2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
 
+		for (divisor = 3; divisor < (prime /2); divisor += 2)
+		{
+			if ((prime % divisor) == 0)
+				prime /= divisor;
+		}
+	}
 
-		
+	printf("%1d\n", prime);
+
+	return (0);
+}		
