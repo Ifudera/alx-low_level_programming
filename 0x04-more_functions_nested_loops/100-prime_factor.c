22c69c2d4d2689d7705ca796234c36617fb7122d
Ifudera <angelifunanya2001@gmail.com>
2023-07-21 03:11:45 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - func prints the largest prime
+ *        factor of the number 612852475143.
+ *
+ * Return: 0 all the time
+ */
+
+int main(void)
+{
+	long prime = 612852475143, divisor;
+
+	while (divisor < (prime / 2))
+
+
+		
